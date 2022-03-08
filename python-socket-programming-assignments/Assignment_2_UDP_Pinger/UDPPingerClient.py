33f516b8f4fb19d11114d72ebb83a3f83045479b
Miical <879754743@qq.com>
2022-03-08 19:51:21 +0800

    Caculate RTT

--- python-socket-programming-assignments/Assignment_2_UDP_Pinger/UDPPingerClient.py
+++ python-socket-programming-assignments/Assignment_2_UDP_Pinger/UDPPingerClient.py
@@ -16,4 +16,4 @@ for i in range(1, 11):
     print('Request timed out')
     clientSocket.close() 
   else:
-    print(rec_message.decode())
+    print(rec_message.decode(), ', RTT =', time() - startTime, 's')

16dae26ee730bc8d4895f560f04c4c9e617cde55
Miical <879754743@qq.com>
2022-03-08 19:41:19 +0800

    Add assignment_2_UDP_Pinger

---
+++ python-socket-programming-assignments/Assignment_2_UDP_Pinger/UDPPingerClient.py
@@ -0,0 +1,19 @@
+from socket import *
+from time import sleep, time
+
+ServerAddress = ('127.0.0.1', 12000)
+
+for i in range(1, 11):
+  startTime = time()
+  message = f"Ping {i} {startTime}"
+  clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+  try:
+    clientSocket.settimeout(1.0)
+    clientSocket.sendto(message.encode(), ServerAddress)
+    rec_message, address = clientSocket.recvfrom(1024)
+  except timeout:
+    print('Request timed out')
+    clientSocket.close() 
+  else:
+    print(rec_message.decode())
